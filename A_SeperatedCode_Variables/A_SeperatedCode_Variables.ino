603b6ab3524b4e50aeb45ffbac47e8caa65a24c1
ahuffman24 <axh340@psu.edu>
2022-03-30 01:08:21 -0400

    Update
    Deleted old code and renamed newest code folder to allow all tabs to be opened at once

--- A_SeperatedCode_Variables/A_SeperatedCode_Variables.ino
+++ A_SeperatedCode_Variables/A_SeperatedCode_Variables.ino
@@ -1,14 +1,42 @@
+
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h> 
+
+//to test all code is working together
+
+
+
+//sda = 20, scl = 21 for mega board i2c module
+
 #define CLK 3 //clock pin on rotary encoder
-#define DT 20 //pin for direction of rotary encoder
+#define DT 6 //pin for direction of rotary encoder
 #define SW 18 //button on rotary encoder
 #define LED_Others 9 //for any beat that is not beat 1
 #define LED_Beat1 10 //for beat 1
-#define timeSigButton 19 //button to change time signature
+#define buttonPin 19 //button to change time signature
+#define timeSigButton 1
 
+
+//old motor code variables
 //motor pins
 #define dirPin 4
 #define stepPin 5
 #define stepsPerRevolution 240
+
+/*new motor code variables
+#define dirPinPos 2
+#define dirPinNeg 4
+#define stepPin 3
+#define stepsPerRevolution 1500*/
+
+LiquidCrystal_I2C lcd(0x27, 20, 4); //20x4 display, defining the lcd
+bool Hi_Hat_Sig = 0; //signal for hi hat to be turned on or off. it is off initially
+//int Drums_Status = 0; //Other drums with actuators are turned OFF initially
+String Hi_Hat_Status; //string for lcd
+//String Drums_Status_; //string for lcd
+int Fun_called = 0; //keeping track of the lcd calls
+String encdir ="";
+String Curr_Selection = "<-";
 
 int rotaryCounter = 0; //for keeping track of rotary position
 int currentStateCLK; //rotary CLK pin state
@@ -26,3 +54,5 @@ int beatsPerMeasure = 4; //starting beats per measure
 int count = 0; //counter for number of beats played in a measure
 String timeSigValues[4] = {"4/4", "6/8", "3/4", "2/4"}; //time signature values - might be needed for lcd
 int timeSig = 0; //starting time signature case
+
+//interrupt function declarations
--- A_SeperatedCode_Variables/C_Tempo_Function.ino
+++ A_SeperatedCode_Variables/C_Tempo_Function.ino
@@ -1,10 +1,22 @@
-//LED's light up to tempo - will control motors here
-//this function also switches time signature from timeSig variable set in timeSigUpdate();
-void tempo()
+
+void timeSignature()
 {
-  if(startMet == 1) //if rotary button is pressed, start blinking to the tempo
-  {
-    //getting time signature
+
+//This function is the brain of ths system
+//Bascially, updateEncoder and button() are interrupts that interrupt the system anytime a signal is received from the encoder/button
+//encoder and button update what they need to update. But timeSignature() is the only function called in loop() continuously
+//timeSignature() updates the delay values based on the Tempo value and determines the correct timeSignature. These variables are used by every other function
+//it also calls the motor() function, which currently is blinking LED's to mimic the motor. They currently only blink when Hi_Hat_Sig is set to 1 
+//If you look at the Serial Monitor, you can see what the Fun_called value is from the button (1 is tempo, 2 is time sig, 3 is hi hat, 4 is drums but drums doesn't work rn bc i didn't program it)
+//For each Fun_called, you can also see the update of the values on the Serial Monitor. From there I can see that everything is updating correctly
+//However, I'm not sure how to implement the LCD_Menu() function. If I call LCD_Menu, the LED's blink slower than they're supposed to. 
+//There has to be a way to update the LCD screen as the encoders are updating the variables. Almost as if the LCD screen is being interrupted when the button/encoder sends a signal
+
+    
+    //updating tempo
+    delay_val_4 = 60000/Tempo;
+    delay_val6_8 = 20040/Tempo;
+  
     switch(timeSig)
     {
       case 0: //4/4 time
@@ -22,10 +34,11 @@ void tempo()
       case 3: //2/4 time
         delay_val = delay_val_4;
         beatsPerMeasure = 2;
-         break;
+        break;
     }
-    
-    //if on beat 1
+
+//This code is now in motor() for testing purposes
+/*    if on beat 1
     if(count == 0)
     {
       Serial.print("\n delay_val is: ");
@@ -47,12 +60,15 @@ void tempo()
       delay(delay_val-50);
       count++;
     }
-      //if at max number of beats per measure
+      if at max number of beats per measure
       if (count >= beatsPerMeasure)
       {
         count = 0;
       }
   }
+*/
+      //LCD_Menu(); Try commenting and uncommenting this to see the difference in the LED's blinking. Use a metronome app if you need to really see the difference
+      motor();
 
   
 }
