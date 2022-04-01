f07ad9543841a449490ea39b8e26ade54aea8a5a
ahuffman24 <axh340@psu.edu>
2022-04-01 12:57:22 -0400

    Updated Motor Code
    Messing with different timers to get bpm to be correct

--- A_Variables/A_Variables.ino
+++ A_Variables/A_Variables.ino
@@ -30,7 +30,8 @@
 #define Kick_stepPin 50
 
 //step size for motors
-#define stepsPerRevolution 750 //for 120bpm
+//#define stepsPerRevolution 800 //for 120bpm and 1600 microsteps
+#define stepsPerRevolution 100 //
 
 //sda = 20, scl = 21 for mega board i2c module
 LiquidCrystal_I2C lcd(0x27, 20, 4); //20x4 display, defining the lcd
@@ -42,9 +43,29 @@ int Fun_called = 1; //keeping track of the lcd calls
 String encdir ="";
 String Curr_Selection = "<-";
 //for LCD_Menu() timer
+
 unsigned long timeBegin; //holds time at start of LCD_Menu()
 unsigned long timeEnd; //holds time at end of LCD_Menu()
 unsigned long timeToRun = 0; //holds difference between timeBegin and timeEnd
+
+unsigned long timeBeginHiHat_CCW; //holds time at start of LCD_Menu()
+unsigned long timeEndHiHat_CCW; //holds time at end of LCD_Menu()
+unsigned long timeToRunHiHat_CCW = 0; //holds difference between timeBegin and timeEnd
+
+unsigned long timeBeginHiHat_Switch; //holds time at start of LCD_Menu()
+unsigned long timeEndHiHat_Switch; //holds time at end of LCD_Menu()
+unsigned long timeToSwitch_HiHat = 0; //holds difference between timeBegin and timeEnd
+
+unsigned long timeBeginHiHat_CW; //holds time at start of LCD_Menu()
+unsigned long timeEndHiHat_CW; //holds time at end of LCD_Menu()
+unsigned long timeToRunHiHat_CW = 0; //holds difference between timeBegin and timeEnd
+
+unsigned long timeBeginKick; //holds time at start of LCD_Menu()
+unsigned long timeEndKick; //holds time at end of LCD_Menu()
+unsigned long timeToRunKick = 0; //holds difference between timeBegin and timeEnd
+
+unsigned long currentMillis;
+unsigned long previousMillis = 0;
 
 int rotaryCounter = 0; //for keeping track of rotary position
 int currentStateCLK; //rotary CLK pin state
@@ -54,10 +75,10 @@ String currentDir =""; //current direction of enncoder
 unsigned long lastButtonPressR = 0; //last press of rotary encoder button
 unsigned long lastButtonPressB = 0; //last press of time signature button
 
-int Tempo = 75; //holds the starting tempo - based on step size
+int Tempo = 120; //holds the starting tempo - based on step size
 int delay_val_4 = 500; //initial delay for tempo of 120 and quarter note gets beat (x/4 time)
 int delay_val6_8 = 167; //initial delay for tempo of 120 and 1/8 note triplet gets beat (x/8 time)
-int delay_val = delay_val_4; //holds value for LED's
+unsigned long delay_val = delay_val_4; //holds value for LED's
 int beatsPerMeasure = 4; //starting beats per measure
 int count = 0; //counter for number of beats played in a measure
 String timeSigValues[4] = {"4/4", "6/8", "3/4", "2/4"}; //time signature values - might be needed for lcd
--- A_Variables/D_MotorCode.ino
+++ A_Variables/D_MotorCode.ino
@@ -1,5 +1,7 @@
 void motor()
 {
+
+  
   //if the hi-hat selection is set to "ON" (Hi_Hat_Sig is 1), turn motor on
   if(Hi_Hat_Sig == 0)
   {
@@ -34,41 +36,86 @@ void motor()
       }
 
    */
+
+   Serial.print("Delay val in seconds: ");
+   Serial.print(delay_val);
+
+   Serial.print("\n\n");
    
       //Hi Hat motor code
-      digitalWrite(HiHat_dirPinPos, LOW);
+      digitalWrite(HiHat_dirPinPos, LOW); //ccw
       //digitalWrite(Kick_dirPinPos, HIGH);
-      Serial.print("stepper 1 rev quickly counterclockwise");
-      Serial.print("\n");
+     
     // Spin the stepper motor 1 revolution quickly:
+      timeBeginHiHat_CCW = millis();
       for (int i = 0; i < stepsPerRevolution; i++) 
       {
         // These four lines result in 1 step:
         digitalWrite(HiHat_stepPin, HIGH);
         //digitalWrite(Kick_stepPin, HIGH);
-        delayMicroseconds(delay_val);
+        delayMicroseconds(2500);
         digitalWrite(HiHat_stepPin, LOW);
         //digitalWrite(Kick_stepPin, LOW);
-        delayMicroseconds(delay_val);
+        //delayMicroseconds(5000);
       }
+   
+      timeEndHiHat_CCW = millis();
+      timeToRunHiHat_CCW = timeEndHiHat_CCW-timeBeginHiHat_CCW;
 
-      digitalWrite(HiHat_dirPinPos, HIGH);
-      //digitalWrite(Kick_dirPinPos, LOW);
+    Serial.print("Hi Hat CCW time is: ");
+    Serial.print(timeToRunHiHat_CCW);
+    Serial.print("\n\n");
+
+   
       
-      Serial.print("stepper 1 rev quickly counterclockwise");
-      Serial.print("\n");
-    // Spin the stepper motor 1 revolution quickly:
+//      Serial.print("Time to turn is: ");
+//      Serial.print(timeToRunHiHat);
+//      Serial.print("\n");
+      
+      timeBeginHiHat_Switch = micros();
+      digitalWrite(HiHat_dirPinPos, HIGH); //cw
+      timeEndHiHat_Switch = micros();
+      
+      
+      timeToSwitch_HiHat = (timeEndHiHat_Switch - timeBeginHiHat_Switch);
+          Serial.print("Time to switch states: ");
+          Serial.print(timeToSwitch_HiHat);
+          Serial.print("\n\n");
+          
+//      //digitalWrite(Kick_dirPinPos, LOW);
+
+    // delay(delay_val-timeToRunHiHat_CCW);
+     
+    Serial.print("Total delay between beats 1 and 2: ");
+    Serial.print(delay_val-timeToRunHiHat_CCW-timeToSwitch_HiHat/1000);
+    Serial.print("\n\n");
+        timeBeginHiHat_CW = millis();
+//    // Spin the stepper motor 1 revolution quickly:
+
+      if(millis()-previousMillis == delay_val)
+      {
       for (int i = 0; i < stepsPerRevolution; i++) 
       {
         // These four lines result in 1 step:
         digitalWrite(HiHat_stepPin, HIGH);
         //digitalWrite(Kick_stepPin, HIGH);
-        delayMicroseconds(delay_val);
+        delayMicroseconds(2500);
         digitalWrite(HiHat_stepPin, LOW);
         //digitalWrite(Kick_stepPin, LOW);
-        delayMicroseconds(delay_val);
+        //delayMicroseconds(5000);
       }
-      
+      }
+       timeEndHiHat_CW = millis();
+      timeToRunHiHat_CW= timeEndHiHat_CW-timeBeginHiHat_CW;
+
+      Serial.print("Time to run HiHat CW: ");
+      Serial.print(timeToRunHiHat_CW);
+      Serial.print("\n\n");
+     // delay(delay_val-timeToRunHiHat_CW);
+      //delay(delay_val);
+      Serial.print("Total time between beats 2 and 3: ");
+      Serial.print(delay_val-timeToRunHiHat_CW);
+      Serial.print("\n\n");
   }
 
   
