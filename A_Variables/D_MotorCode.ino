3538c45002855389db9a14a31a5b0774485e3ab4
ahuffman24 <axh340@psu.edu>
2022-04-07 08:00:39 -0400

    Updated motor code

--- A_Variables/D_MotorCode.ino
+++ A_Variables/D_MotorCode.ino
@@ -112,41 +112,6 @@ if(Hi_Hat_Sig == 0 && Kick_Sig == 1)
 
   if(Kick_Sig == 0 && Hi_Hat_Sig == 1)
   {
-    /*
-   Serial.print("Delay val in seconds: ");
-   Serial.print(delay_val);
-   Serial.print("\n\n");
-    //LED Testing - uncomment to test tempo with LED's
-    
-    if(count == 0)
-      {
-        Serial.print("\n delay_val is: ");
-        Serial.print(delay_val);
-        Serial.print("\n\n");
-        digitalWrite(LED_Beat1, HIGH);
-        delay(50);
-        digitalWrite(LED_Beat1, LOW);
-        delay(delay_val-50);
-        count++;
-      }
-  
-      //if on any other beat
-     else
-      {
-        digitalWrite(LED_Others, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(50);                       // wait for a second
-        digitalWrite(LED_Others, LOW);    // turn the LED off by making the voltage LOW
-        delay(delay_val-50);
-        count++;
-      }
-
-      if (count >= beatsPerMeasure)
-      {
-        count = 0;
-      }
-
-   */
-
    Serial.print("Delay val in seconds: ");
    Serial.print(delay_val);
    Serial.print("\n\n");
@@ -221,40 +186,6 @@ if(Hi_Hat_Sig == 0 && Kick_Sig == 1)
   
   if(Hi_Hat_Sig == 0 && Kick_Sig == 0)
   {
-    /*
-   Serial.print("Delay val in seconds: ");
-   Serial.print(delay_val);
-   Serial.print("\n\n");
-    //LED Testing - uncomment to test tempo with LED's
-    
-    if(count == 0)
-      {
-        Serial.print("\n delay_val is: ");
-        Serial.print(delay_val);
-        Serial.print("\n\n");
-        digitalWrite(LED_Beat1, HIGH);
-        delay(50);
-        digitalWrite(LED_Beat1, LOW);
-        delay(delay_val-50);
-        count++;
-      }
-  
-      //if on any other beat
-     else
-      {
-        digitalWrite(LED_Others, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(50);                       // wait for a second
-        digitalWrite(LED_Others, LOW);    // turn the LED off by making the voltage LOW
-        delay(delay_val-50);
-        count++;
-      }
-
-      if (count >= beatsPerMeasure)
-      {
-        count = 0;
-      }
-
-   */
 
    Serial.print("Delay val in seconds: ");
    Serial.print(delay_val);
@@ -333,38 +264,3 @@ if(Hi_Hat_Sig == 0 && Kick_Sig == 1)
 
 
 }
-
-
-
-
-//{
-//
-//
-//
-//  // clockwise
-//    digitalWrite(dirPin, HIGH);
-//
-//  // Spin the stepper motor 1 revolution slowly:
-//  for (int i = 0; i < stepsPerRevolution; i++) 
-//  {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(delay_val);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(delay_val);
-//  }
-//
-//delay(50);
-//
-//  //counter-clockwise
-//    digitalWrite(dirPin, LOW);
-//
-//  for (int i = 0; i < stepsPerRevolution; i++) 
-//  {
-//      // These four lines result in 1 step:
-//      digitalWrite(stepPin, HIGH);
-//      delayMicroseconds(delay_val);
-//      digitalWrite(stepPin, LOW);
-//      delayMicroseconds(delay_val);
-//  }
-//}
